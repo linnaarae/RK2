1d7670a874d5aa1327a58a12f22bf52396e1a4c8
linnaarae <71868736+linnaarae@users.noreply.github.com>
2021-06-01 11:35:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.menu-burger-header').click(function() {
+        $('.menu-burger-header').toggleClass('open-menu');
+        $('.header-container-nav').toggleClass('open-menu');
+    });
+});
